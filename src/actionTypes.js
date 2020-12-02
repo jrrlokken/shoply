49fa3b03e5929dee57ce3994088dfa0125d08365
Joshua Lokken <jrrlokken@gmail.com>
2020-12-02 14:12:14 -0600

    Remove unnecessary action types

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,5 +1,3 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const APPLY_DISCOUNT = "APPLY_DISCOUNT";
-export const INCREASE_QTY = "INCREASE_QTY";
-export const DECREASE_QTY = "DECREASE_QTY";
