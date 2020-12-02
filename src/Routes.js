9c6828452ad701a45e0cfafc89f515767d865a65
Joshua Lokken <jrrlokken@gmail.com>
2020-12-02 14:11:29 -0600

    Add application routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,1 +1,23 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
+import ProductList from './ProductList';
+import ProductDetail from './ProductDetail';
+import Cart from './Cart';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path='/' exact>
+        <ProductList />
+      </Route>
+      <Route path='/products/:id' exact>
+        <ProductDetail />
+      </Route>
+      <Route path='/cart' exact>
+        <Cart />
+      </Route>
+      <Redirect to='/' />
+    </Switch>
+  );
+}
+
+export default Routes;
