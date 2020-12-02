4243f31e3ddaf4501211bdf31b1696bfdf21528c
Joshua Lokken <jrrlokken@gmail.com>
2020-12-02 14:36:22 -0600

    Add id as payload to actions

--- src/actions.js
+++ src/actions.js
@@ -1,22 +1,20 @@
 import {
   ADD_TO_CART,
   REMOVE_FROM_CART,
-  INCREASE_QTY,
-  DECREASE_QTY,
   APPLY_DISCOUNT
 } from './actionTypes';
 
 export function addItem(id) {
   return {
     type: ADD_TO_CART,
-    product
+    id
   }
 }
 
 export function removeItem(id) {
   return {
     type: REMOVE_FROM_CART,
-    product
+    id
   }
 }
 
