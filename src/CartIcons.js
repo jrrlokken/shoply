eb473231c0e49dd6c79fafabd19d0f37dfef59ba
Joshua Lokken <jrrlokken@gmail.com>
2020-12-02 13:45:14 -0600

    Implement cart icons

---
+++ src/CartIcons.js
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { addItem, removeItem } from './actions';
+
+const CartIcons = ({ id }) => {
+  const dispatch = useDispatch();
+  const add = () => dispatch(addItem(id));
+  const remove = () => dispatch(removeItem(id));
+
+  return (
+    <div className="d-flex justify-content-between">
+      <i
+        onClick={add}
+        className="CartIcon fas fa-cart-plus fa-2x text-success"
+      />
+      <i
+        onClick={remove}
+        className="CartIcon fas fa-cart-minus fa-2x text-danger"
+      />
+    </div>
+  );
+}
+
+export default CartIcons;
