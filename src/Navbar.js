7adcf39a1597c121140e5f872651f71ff52c092b
Joshua Lokken <jrrlokken@gmail.com>
2020-12-02 14:11:17 -0600

    Add navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,6 +8,22 @@ const Navbar = () => {
   const itemUnit = itemCount === 1 ? "item" : "items";
 
   return (
-    
-  )
+    <nav className="navbar navbar-light bg-info">
+      <Link to='/' className="navbar-brand text-light">Shoply</Link>
+      <ul className="navbar-nav flex-row">
+        <li className="nav-item pr-3">
+          <span className="navbar-text text-light">
+            {itemCount} {itemUnit} (${cartTotal})
+          </span>
+        </li>
+        <li className="nav-item">
+          <Link to="/cart" className="nav-link text-light">
+            Cart
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
+export default Navbar;
